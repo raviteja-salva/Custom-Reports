4292b0bca0fd3e0baae43789c7acf00dc088ea4a
raviteja <ravitejasalva@gmail.com>
2024-08-25 17:05:33 +0530

    fix issues in CR

--- src/components/SavedReportsList.js
+++ src/components/SavedReportsList.js
@@ -19,11 +19,11 @@ const SavedReportsList = ({ savedReports, onExport, onDelete, onEdit }) => (
   <Card>
     <CardTitle>Saved Reports</CardTitle>
     {savedReports.map((report) => (
-      <SavedReportItem key={report.id}>
+      <SavedReportItem key={report._id}>
         <SavedReportName>{report.name}</SavedReportName>
         <ButtonGroup>
           <Button onClick={() => onEdit(report)}>Edit</Button>
-          <Button onClick={() => onDelete(report.id)}>Delete</Button>
+          <Button onClick={() => onDelete(report._id)}>Delete</Button>
           <Button onClick={() => onExport('pdf', report)}>Export PDF</Button>
           <Button onClick={() => onExport('excel', report)}>Export Excel</Button>
           <Button onClick={() => onExport('csv', report)}>Export CSV</Button>
